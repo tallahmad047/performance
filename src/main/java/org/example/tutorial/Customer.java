00ef81d2e01ff1a453bdfbdad805b9e8a9969de3
mbackesow1 <mbackesow43@gmail.com>
2023-08-10 23:55:52 +0200

    Creation du  datasets  des 8  fils

--- src/main/java/org/example/tutorial/Customer.java
+++ src/main/java/org/example/tutorial/Customer.java
@@ -6,6 +6,7 @@ class Customer{
     double endServiceTime;
     double waitingTime;
     int type;
+    //la longueur   des  8  filsles  plus  frequents
     int srv1,srv2, srv3, srv4, srv5,srv6,srv7,srv8;
     int  nbServeurs;
     double LES;
